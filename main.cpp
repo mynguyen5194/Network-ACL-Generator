722aa7086df16bcae9d4dec93a30a29d77ab3f7a
mynguyen5194 <mydng@ucdavis.edu>
2018-06-05 18:42:22 -0700

    Implement and test parseAlertFile()

--- main.cpp
+++ main.cpp
@@ -1,9 +1,73 @@
 
 #include <iostream>
+#include <fstream>
+#include "Alert.hpp"
+
+//    void setAlert(string timestamp, int signatureID,
+//string message, string protocol, string sourceIP,
+//int sourcePort, string destinationIP, int destinationPort);
 
 using namespace std;
 
+bool parseAlertFile(string filePath) {
+    ifstream file;
+    string line, delimiter = ",";
+    string timestamp = "", message = "",
+        protocol = "", sourceIP = "", destinationIP = "";
+    int signatureID = 0, sourcePort = 0, destinationPort = 0;
+    size_t pos = 0;
+    Alert alertInfo;
+    
+    file.open(filePath);
+    if (!file) {
+        cerr << "Error opening file alert.csv\n";
+        return false;
+    }
+    
+    while (getline(file, line)) {
+        pos = line.find(delimiter);
+        timestamp = line.substr(0, pos);
+        line.erase(0, pos + delimiter.length());
+        
+        pos = line.find(delimiter);
+        signatureID = atoi(line.substr(0, pos).c_str());
+        line.erase(0, pos + delimiter.length());
+        
+        pos = line.find(delimiter);
+        message = line.substr(0, pos);
+        line.erase(0, pos + delimiter.length());
+        
+        pos = line.find(delimiter);
+        protocol = line.substr(0, pos);
+        line.erase(0, pos + delimiter.length());
+        
+        pos = line.find(delimiter);
+        sourceIP = line.substr(0, pos);
+        line.erase(0, pos + delimiter.length());
+        
+        pos = line.find(delimiter);
+        sourcePort = atoi(line.substr(0, pos).c_str());
+        line.erase(0, pos + delimiter.length());
+        
+        pos = line.find(delimiter);
+        destinationIP = line.substr(0, pos);
+        line.erase(0, pos + delimiter.length());
+        
+        pos = line.find(delimiter);
+        destinationPort = atoi(line.substr(0, pos).c_str());
+        line.erase(0, pos + delimiter.length());
+        
+        alertInfo.setAlert(timestamp, signatureID, message,
+            protocol, sourceIP, sourcePort, destinationIP, destinationPort);
+        
+        alertInfo.printAlert();
+        
+    }
+    return true;
+}
+
 int main(int argc, const char * argv[]) {
+    parseAlertFile("/Users/MyNguyen/Desktop/152C/Project/Network\ ACL\ Generator/Network\ ACL\ Generator/alertTest.csv");
     
     return 0;
 }
